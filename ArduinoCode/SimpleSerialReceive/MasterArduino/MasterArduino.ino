bc005803bfcfd2129a25cee3797a45127a3f83c2
Arno Laurie <87619843+Netfluxx@users.noreply.github.com>
2024-05-18 20:45:02 +0200

    Update MasterArduino.ino with mutliple slave and error detection, needs to be tested

--- ArduinoCode/SimpleSerialReceive/MasterArduino/MasterArduino.ino
+++ ArduinoCode/SimpleSerialReceive/MasterArduino/MasterArduino.ino
@@ -1,87 +1,134 @@
 #include <Wire.h>
+#include <vector>
 
-// Commands
-const byte CMD_FORWARD = 0x01;
+// command identifiers
+const byte CMD_FORWARD  = 0x01;
 const byte CMD_BACKWARD = 0x02;
-const byte CMD_RIGHT = 0x03;
-const byte CMD_LEFT = 0x04;
-const byte CMD_STOP = 0x05;
-const byte CMD_ERR = 0x0f;
+const byte CMD_RIGHT    = 0x03;
+const byte CMD_LEFT     = 0x04;
+const byte CMD_STOP     = 0x05;
 
-// Slave addresses
-const byte SLAVE_ADDRESSES[] = {0x10, 0x20, 0x30, 0x40};
-const int NUM_SLAVES = sizeof(SLAVE_ADDRESSES) / sizeof(SLAVE_ADDRESSES[0]);
+// I2C slave addresses
+const byte SLAVE_1 = 0x10;  // Front Right Arduino
+const byte SLAVE_2 = 0x20;  // Front Left  Arduino
+const byte SLAVE_3 = 0x30;  // Back  Right Arduino
+const byte SLAVE_4 = 0x40;  // Back  Left  Arduino
+
+const byte CMD_ERR = 0xff;
+const byte SLV_ERR = 0xfe;
+
+byte slave_addr = SLV_ERR;
+byte command = CMD_ERR;
+const String spd_err = "SPEED_ERR"; // couldn't send data to slave
+const String fb_err = "SLV_FB_ERR"; // slave didn't respond
+const String distrib_err = "DIST_ERR"; // error when distributing the command to the slaves
+
+// message format to send back to the RPi (comma separated): 
+// received_cmd (from master arduino), f_r_send, f_r_fb, f_r_speed, f_l_send, f_l_fb, f_l_speed, b_r_send, b_r_fb, b_r_speed, b_l_send, b_l_fb, b_l_speed
+// where:
+// received_cmd = "f" or "b" or "r" or "l" or "s" or "#"
+// x_y_send = "OK" or "DIST_ERR"
+// x_y_fb   = "OK" or "SLV_FB_ERR"
+// x_y_speed = String(float value) or "SPEED_ERR" 
+
+std::vector<String> received_speeds = {spd_err, spd_err, spd_err, spd_err}; // speed values received from the slaves (0-255)
+std::vector<String> SlaveSendError = {distrib_err, distrib_err, distrib_err, distrib_err}; // error when sending the command to the slaves (OK or DIST_ERR)
+std::vector<String> SlaveReceiveError = {fb_err, fb_err, fb_err, fb_err}; // error when receiving the feedback from the slaves (OK or SLV_FB_ERR)
+
+String state_feedback = fb_err; // custom feedback message to send to the RPi according to the format above
 
 void setup() {
-  Serial.begin(9600); // Start serial communication
-  while (!Serial); // Wait for serial port to connect
-  Wire.begin(); // Start I2C as master
-  delay(3000); // Initial delay for startup
+  Wire.begin();
+  Serial.begin(9600);
+  delay(1500);
 }
 
 void loop() {
-  String state_package = "";
   if (Serial.available() > 0) {
     String received_str = Serial.readStringUntil('\n');
-    delay(10); // ensure receptio  // while (Wire.available()){
-  //   byte command = Wire.read();
-  //   if(command == CMD_ERR){
-  //     executeCommand(prev_command);
-  //   }else{
-  //     executeCommand(command);
-  //     prev_command = command;
-  //   }
-  // }
-  //     }
-  //     // Send command to each slave
-  //     Wire.beginTransmission(SLAVE_ADDRESSES[i]);
-  //     Wire.write(command);
-  //     Wire.endTransmission();
-  //     //no receiving feedback for now
-  //     // Request state from each slave
-  //     //Wire.requestFrom((int)SLAVE_ADDRESSES[i], sizeof(long));
-  //     //while(Wire.available() < sizeof(long)) {
-  //     //  ; // Wait until all bytes are received
-  //     //}
+    delay(6); // 6ms
+    
+    if (received_str == "forward") {
+      state_feedback = "f,";
+      command = CMD_FORWARD;
+    } else if (received_str == "backwards") {
+      state_feedback = "b,";
+      command = CMD_BACKWARD;
+    } else if (received_str == "right") {
+      state_feedback = "r,";
+      command = CMD_RIGHT;
+    } else if (received_str == "left") {
+      state_feedback = "l,";
+      command = CMD_LEFT;
+    } else if (received_str == "stop") {
+      state_feedback = "s,";
+      command = CMD_STOP;
+    } else {
+      command = CMD_ERR;
+      state_feedback = "#,";
+    }
+  
+    for (unsigned int slave_index = 1; slave_index <= 4; slave_index++) {
+      switch (slave_index) {
+        case 1:
+          slave_addr = SLAVE_1;
+          break;
+        case 2:
+          slave_addr = SLAVE_2;
+          break;
+        case 3:
+          slave_addr = SLAVE_3;
+          break;
+        case 4:
+          slave_addr = SLAVE_4;
+          break;
+        default:
+          slave_addr = SLV_ERR;
+          break;
+      }
 
-  //     //long receivedValue = 0;
-  //     //for (int j = 0; j < sizeof(long); j++) {
-  //     //  receivedValue |= ((long)Wire.read()) << (j * 8); // Reconstruct the long from bytes
-  //     //}  // while (Wire.available()){
-  //   byte command = Wire.read();
-  //   if(command == CMD_ERR){
-  //     executeCommand(prev_command);
-  //   }else{
-  //     executeCommand(command);
-  //     prev_command = command;
-  //   }
-  // }
-  //   }
+      if (slave_addr != SLV_ERR) {
+        Wire.beginTransmission(slave_addr);
+        Wire.write(command);
+        byte error = Wire.endTransmission();
 
-  //   // Send the collected states to Raspberry Pi
-  //   //Serial.println(state_package);  //No feedback for now
+        if (error) {
+          received_speeds[slave_index - 1] = spd_err;
+          SlaveSendError[slave_index - 1] = distrib_err;
+          SlaveReceiveError[slave_index - 1] = fb_err;
+          continue;
+        } else {
+          SlaveSendError[slave_index - 1] = "OK";
+        }
+
+        Wire.requestFrom(slave_addr, 1);  // request 1 byte from the slave Arduino
+        if (Wire.available() == 0) { // no response from slave
+          received_speeds[slave_index - 1] = spd_err;
+          SlaveReceiveError[slave_index - 1] = fb_err;
+        } else if (Wire.available() > 0) {
+          byte state = Wire.read();
+          received_speeds[slave_index - 1] = String(state);
+          SlaveReceiveError[slave_index - 1] = "OK";
+        }
+      }
+    }
+
+    for (unsigned int i = 0; i < 4; i++) {
+      state_feedback += SlaveSendError[i] + ",";
+      state_feedback += SlaveReceiveError[i] + ",";
+      state_feedback += received_speeds[i] + ",";
+    }
+    Serial.println(state_feedback);
+
+    // reset for next command
+    state_feedback = fb_err;
+    slave_addr = SLV_ERR;
+    command = CMD_ERR;
+    received_speeds = {spd_err, spd_err, spd_err, spd_err};
+    SlaveSendError = {distrib_err, distrib_err, distrib_err, distrib_err};
+    SlaveReceiveError = {fb_err, fb_err, fb_err, fb_err};
   }
-  delay(10);
-}
 
-byte determineCommand(String input) {
-  if(input == "forward") {
-    Serial.println("f");
-    return CMD_FORWARD;
-  } else if(input == "backwards") {
-    Serial.println("b");
-    return CMD_BACKWARD;
-  } else if(input == "right") {
-    Serial.println("r");
-    return CMD_RIGHT;
-  } else if(input == "left") {
-    Serial.println("l");
-    return CMD_LEFT;
-  } else if(input == "stop") {
-    Serial.println("s");
-    return CMD_STOP;
-  } else {
-    Serial.println("#");
-    return CMD_ERR;
-  }
+  delay(6); // 6ms
+
 }
